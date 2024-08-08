c6b31c729a8e363d6ffff5cff00b8723c8bb007a
aliha <alihajji315@gmail.com>
2024-08-08 18:03:26 +0100

    Controllers

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace api.Controllers
+{
+    [Route("api/StockModel")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+       private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetALL(){
+            var stocks = _context.StockModel.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("id")]
+    }
+}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1835a21abaf413abf313becc2150ced84654c6c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503908dccace6596ffd9bf6998dc415ab805087d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
